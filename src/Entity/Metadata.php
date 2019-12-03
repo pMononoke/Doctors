0da20dc8cbfec61fbbde278b7fb64b880731d0c5
pMononoke <orensource.com@gmail.com>
2019-12-03 18:30:31 +0100

    Refactoring entity Metadata.

--- src/Entity/Metadata.php
+++ src/Entity/Metadata.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Metadata
+ * Metadata.
  *
  * @ORM\Table(name="metadata")
  * @ORM\Entity(repositoryClass="App\Repository\MetadataRepository")
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Metadata
 {
     /**
-     * @var integer
+     * @var int
      *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
@@ -34,96 +34,52 @@ class Metadata
      * @ORM\Column(name="thevalue", type="string", length=255)
      */
     private $thevalue;
-    
+
     /**
-    * @ORM\ManyToOne(targetEntity="App\Entity\Test", inversedBy="metadata")
-    * @ORM\JoinColumn(name="test_id", referencedColumnName="id", nullable=false)
-    */
+     * @ORM\ManyToOne(targetEntity="App\Entity\Test", inversedBy="metadata")
+     * @ORM\JoinColumn(name="test_id", referencedColumnName="id", nullable=false)
+     */
     private $test;
-    
+
     /************ constructeur ************/
-    
+
     public function __construct()
     {
     }
-    
+
     /************ getters & setters  ************/
 
-   /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * Set thekey
-     *
-     * @param string $thekey
-     * @return Metadata
-     */
-    public function setThekey($thekey)
+    public function setThekey(string $thekey): void
     {
         $this->thekey = $thekey;
-
-        return $this;
     }
 
-    /**
-     * Get thekey
-     *
-     * @return string 
-     */
-    public function getThekey()
+    public function getThekey(): string
     {
         return $this->thekey;
     }
 
-    /**
-     * Set thevalue
-     *
-     * @param string $thevalue
-     * @return Metadata
-     */
-    public function setThevalue($thevalue)
+    public function setThevalue(string $thevalue): void
     {
         $this->thevalue = $thevalue;
-
-        return $this;
     }
 
-    /**
-     * Get thevalue
-     *
-     * @return string 
-     */
-    public function getThevalue()
+    public function getThevalue(): string
     {
         return $this->thevalue;
     }
 
-    /**
-     * Set test
-     *
-     * @param \App\Entity\Test $test
-     * @return Metadata
-     */
-    public function setTest(\App\Entity\Test $test)
+    public function setTest(Test $test): void
     {
         $this->test = $test;
-
-        return $this;
     }
 
-    /**
-     * Get test
-     *
-     * @return \App\Entity\Test
-     */
-    public function getTest()
+    public function getTest(): ?Test
     {
         return $this->test;
     }
