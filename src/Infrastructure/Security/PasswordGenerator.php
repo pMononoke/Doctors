f6bda3f7d386de7cac74a41fe7c7edd2080ca35a
pMononoke <francescabonadonna.gmail.com>
2020-01-16 13:56:55 +0100

    Add password generator.

---
+++ src/Infrastructure/Security/PasswordGenerator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zero
+ * Date: 16/01/20
+ * Time: 13.31.
+ */
+
+namespace App\Infrastructure\Security;
+
+class PasswordGenerator
+{
+}
---
+++ src/Service/PasswordGenerator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zero
+ * Date: 16/01/20
+ * Time: 13.32.
+ */
+
+namespace App\Service;
+
+interface PasswordGenerator
+{
+}
